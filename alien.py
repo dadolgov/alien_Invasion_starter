7eae7d81c9f83892c6c91a777c5c60522ff4f7eb
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-04-16 08:54:21 -0400

    p2-created a single row of stationary aliens for alien fleet

--- alien.py
+++ alien.py
@@ -7,7 +7,7 @@ from pygame.sprite import Sprite
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from alien_invasion import AlienInvasion
+    from alien_fleet import AlienFleet
 
 class Alien(Sprite):
     """Holds settings and methods for a standard bullet
@@ -15,7 +15,7 @@ class Alien(Sprite):
     Args:
         Sprite : image of a bullet, .png
     """
-    def __init__(self, game:"AlienInvasion", x:float, y:float)->None:
+    def __init__(self, fleet:"AlienFleet", x:float, y:float)->None:
         """references the screen and game settings, links the sprites,
         sets up the size of the sprite used
 
@@ -25,9 +25,10 @@ class Alien(Sprite):
             y(float): vertical coordinate
         """
         super().__init__()
-        self.screen=game.screen
-        self.boundaries=game.screen.get_rect()
-        self.settings=game.settings
+
+        self.screen=fleet.game.screen
+        self.boundaries=fleet.game.screen.get_rect()
+        self.settings=fleet.game.settings
 
         self.image=pygame.image.load(self.settings.alien_file)
         self.image=pygame.transform.scale(self.image,(self.settings.alien_w, self.settings.alien_h))
